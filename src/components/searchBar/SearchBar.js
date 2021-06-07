e0ebf2152ebfdbe0f65f4cdf60bb23b8388b1f3f
dekkersbd <79840065+dekkersbd@users.noreply.github.com>
2021-06-07 12:30:20 +0200

    Hoofdstuk 6 Edhub finished

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css';
 
-function SearchBar() {
-  return (
+
+function SearchBar({setLocationHandler}) {
+    const [query, setQuery] = useState("");
+
+    function handleClick () {
+        setLocationHandler(query);
+    }
+
+    function keypressCheck(e) {
+        if (e.keyCode === 13) {
+            setLocationHandler(query);
+        }
+    }
+
+    return (
     <span className="searchbar">
       <input
         type="text"
         name="search"
         placeholder="Zoek een stad in Nederland"
+        value={query}
+        onKeyDown={keypressCheck}
+        onChange={(e)=> setQuery(e.target.value)}
       />
 
-      <button type="button">
+      <button type="button"
+      onClick={handleClick}
+      >
         Zoek
       </button>
     </span>
